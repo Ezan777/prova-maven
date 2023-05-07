d203d968361b3817b58f06a2be4fdce5c2e5d169
Enrico <enrizangri@gmail.com>
2023-05-07 14:57:47 +0200

    Initial commit

---
+++ roman-number/src/main/java/it/unipd/mtss/App.java
@@ -0,0 +1,17 @@
+////////////////////////////////////////////////////////////////////
+// ENRICO ZANGRANDO 2000547
+////////////////////////////////////////////////////////////////////
+
+package it.unipd.mtss;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
