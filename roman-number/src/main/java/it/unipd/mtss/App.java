7e6b1f2a477179455c7f417b52056988119fd2bd
Enrico <enrizangri@gmail.com>
2023-05-07 16:17:15 +0200

    Added jacoco and some tests

--- roman-number/src/main/java/it/unipd/mtss/App.java
+++ roman-number/src/main/java/it/unipd/mtss/App.java
@@ -10,6 +10,54 @@ package it.unipd.mtss;
  */
 public class App 
 {
+    public String getName() {
+        return "Enrico";
+    }
+
+    public String getSurname() {
+        return "Zangrando";
+    }
+
+    public int getIdNumber() {
+        return 2000547;
+    }
+
+    public int getAge() {
+        return 23;
+    }
+
+    public String presentation() {
+        return "I am Enrico Zangrando, id 2000547 and I am 23 years old";
+    }
+
+    public String getDogName() {
+        return "Peggy";
+    }
+
+    public String getFavouriteGame() {
+        return "RDR2";
+    }
+
+    public String getFavouriteStarWars() {
+        return "Revenge of the Sith";
+    }
+
+    public String getFavouriteCartoon() {
+        return "Kung Fu Panda";
+    }
+
+    public String getFavouriteConsole() {
+        return "PS5";
+    }
+
+    public String getFavouriteLinux() {
+        return "Arch";
+    }
+
+    public String getFavouriteMobileOS() {
+        return "Android";
+    }
+
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
--- roman-number/src/test/java/it/unipd/mtss/AppTest.java
+++ roman-number/src/test/java/it/unipd/mtss/AppTest.java
@@ -1,14 +1,18 @@
 package it.unipd.mtss;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+
+import java.util.Objects;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
 {
+    App app = new App();
     /**
      * Rigorous Test :-)
      */
@@ -16,5 +20,65 @@ public class AppTest
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    }
+
+    @Test
+    public void testName() {
+        assertEquals("Enrico", app.getName());
+    }
+
+    @Test
+    public void testSurname() {
+        assertEquals("Zangrando", app.getSurname());
+    }
+
+    @Test
+    public void testIdNumber() {
+        assertEquals(2000547, app.getIdNumber());
+    }
+
+    @Test
+    public void testAge() {
+        assertEquals(23, app.getAge());
+    }
+
+    @Test
+    public void testPresentation() {
+        assertEquals("I am Enrico Zangrando, id 2000547 and I am 23 years old", app.presentation());
+    }
+
+    @Test
+    public void testDogName() {
+        assertEquals("Peggy", app.getDogName());
+    }
+
+    @Test
+    public void testStarWars() {
+        assertEquals("Revenge of the Sith", app.getFavouriteStarWars());
+    }
+
+    @Test
+    public void testGame() {
+        assertEquals("RDR2", app.getFavouriteGame());
+    }
+
+    @Test
+    public void testCartoon() {
+        assertEquals("Kung Fu Panda", app.getFavouriteCartoon());
+    }
+
+    @Test
+    public void testConsole() {
+        assertEquals("PS5", app.getFavouriteConsole());
+    }
+
+    @Test
+    public void testLinux() {
+        assertEquals("Arch", app.getFavouriteLinux());
+    }
+
+    @Test
+    public void testMobileOs() {
+        assertEquals("Android", app.getFavouriteMobileOS());
     }
 }
